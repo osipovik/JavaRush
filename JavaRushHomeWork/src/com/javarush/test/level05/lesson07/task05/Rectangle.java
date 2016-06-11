3557ed110c8beadb276a46bdbda189d75c6676aa
Ivan Osipov <osipovik89@gmail.com>
2016-06-11 20:49:02 +0300

    added tasks from level 5, lessons 5,7,9

---
+++ JavaRushHomeWork/src/com/javarush/test/level05/lesson07/task05/Rectangle.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level05.lesson07.task05;
+
+/* Создать класс прямоугольник (Rectangle)
+Создать класс прямоугольник (Rectangle). Его данными будут top, left, width, height (левая координата, верхняя, ширина и высота).
+Создать для него как можно больше методов initialize(…)
+Примеры:
+-	заданы 4 параметра: left, top, width, height
+-	ширина/высота не задана (оба равны 0)
+-	высота не задана (равно ширине) создаём квадрат
+-	создаём копию другого прямоугольника (он и передаётся в параметрах)
+*/
+
+public class Rectangle {
+    private int top,
+                left,
+                width,
+                height;
+
+    public void initialize() {
+
+    }
+
+    public void initialize(int top) {
+
+    }
+
+    public void initialize(int top, int left) {
+
+    }
+
+    public void initialize(int top, int left, int width) {
+
+    }
+
+    public void initialize(int top, int left, int width, int height) {
+
+    }
+}
